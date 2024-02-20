01ab3bad42fd4ff93d745d6bb81266f1bc3d1941
lgf2111 <86937973+lgf2111@users.noreply.github.com>
2024-02-20 14:49:34 +0800

    separate ssg helper, full post page

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,14 +1,46 @@
 import Head from "next/head";
+import { api } from "~/utils/api";
+import { type GetStaticProps } from "next";
+import { PageLayout } from "~/components/layout";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
+import { PostView } from "~/components/postView";
 
-export default function SinglePostPage() {
+export default function SinglePostPage({ id }: { id: string }) {
+  const { data } = api.posts.getById.useQuery({
+    id,
+  });
+
+  if (!data) return <div>Something went wrong</div>;
+
   return (
     <>
       <Head>
-        <title>Post</title>
+        <title>{`${data.post.content} - @${data.author.username}`}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>Post View</div>
-      </main>
+      <PageLayout>
+        <PostView {...data} />
+      </PageLayout>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = generateSSGHelper();
+
+  const id = context.params?.id;
+
+  if (typeof id !== "string") throw new Error("Invalid id");
+
+  await ssg.posts.getById.prefetch({ id });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      id,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
+};
