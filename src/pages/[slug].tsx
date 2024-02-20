2dd8e5e14e6ddd1366eeb85e95e0dfc22cfd0c2c
lgf2111 <86937973+lgf2111@users.noreply.github.com>
2024-02-20 14:36:05 +0800

    render posts on profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -7,6 +7,26 @@ import superjson from "superjson";
 import { type GetStaticProps } from "next";
 import { PageLayout } from "~/components/layout";
 import Image from "next/image";
+import { LoadingPage } from "~/components/loading";
+import { PostView } from "~/components/postView";
+
+const ProfileFeed = (props: { userId: string }) => {
+  const { data, isLoading } = api.posts.getPostsByUserId.useQuery({
+    userId: props.userId,
+  });
+
+  if (isLoading) return <LoadingPage />;
+
+  if (!data || data.length === 0) return <div>No posts</div>;
+
+  return (
+    <div className="flex flex-col">
+      {data.map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
+    </div>
+  );
+};
 
 export default function ProfilePage({ username }: { username: string }) {
   const { data } = api.profile.getUserByUsername.useQuery({
@@ -33,6 +53,7 @@ export default function ProfilePage({ username }: { username: string }) {
         <div className="h-[64px]"></div>
         <div className="p-4 text-2xl font-bold">{`@${data.username ?? ""}`}</div>
         <div className="w-full border-b border-slate-400"></div>
+        <ProfileFeed userId={data.id} />
       </PageLayout>
     </>
   );
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,5 +1,4 @@
 import { clerkClient } from "@clerk/nextjs";
-import { type User } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { Ratelimit } from "@upstash/ratelimit";
@@ -11,6 +10,23 @@ import {
   publicProcedure,
 } from "~/server/api/trpc";
 import { filterUserForClient } from "~/server/helpers/filterUserForClient";
+import { type Post } from "@prisma/client";
+
+const addUserDataToPosts = async (posts: Post[]) => {
+  const users = (
+    await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    })
+  ).map(filterUserForClient);
+
+  return posts.map((post) => {
+    const author = users.find((user) => user.id === post.authorId);
+    if (!author?.username)
+      throw new TRPCError({ code: "NOT_FOUND", message: "Author not found" });
+    return { post, author: { ...author, username: author.username } };
+  });
+};
 
 // Create a new ratelimiter, that allows 3 requests every 1 minute
 const ratelimit = new Ratelimit({
@@ -26,20 +42,20 @@ export const postsRouter = createTRPCRouter({
       orderBy: [{ createdAt: "desc" }],
     });
 
-    const users = (
-      await clerkClient.users.getUserList({
-        userId: posts.map((post) => post.authorId),
-        limit: 100,
-      })
-    ).map(filterUserForClient);
-
-    return posts.map((post) => {
-      const author = users.find((user) => user.id === post.authorId);
-      if (!author?.username)
-        throw new TRPCError({ code: "NOT_FOUND", message: "Author not found" });
-      return { post, author: { ...author, username: author.username } };
-    });
+    return addUserDataToPosts(posts);
   }),
+
+  getPostsByUserId: publicProcedure
+    .input(z.object({ userId: z.string() }))
+    .query(({ ctx, input }) =>
+      ctx.db.post
+        .findMany({
+          where: { authorId: input.userId },
+          take: 100,
+          orderBy: [{ createdAt: "desc" }],
+        })
+        .then(addUserDataToPosts),
+    ),
 
   create: privateProcedure
     .input(
